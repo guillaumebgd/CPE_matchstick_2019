42c582d9cbe23d73ee45804d492e9ef84c9d83bd
gbogard <guillaume.bogard-coquard@epitech.eu>
2020-01-23 19:25:31 +0100

    Update: used to return the wrong error when lines input was set to 0

--- src/game_itterations/get_info_lines.c
+++ src/game_itterations/get_info_lines.c
@@ -19,7 +19,11 @@ static boolean_t check_valid_number(const char *written,
         return (FALSE);
     }
     stock = my_getnbr(written);
-    if (stock <= 0 || stock > nb_lines) {
+    if (stock == 0) {
+        my_putstr(1, "Error: invalid input (positive number expected)\n");
+        return (FALSE);
+    }
+    if (stock < 0 || stock > nb_lines) {
         my_putstr(1, "Error: this line is out of range\n");
         return (FALSE);
     }
