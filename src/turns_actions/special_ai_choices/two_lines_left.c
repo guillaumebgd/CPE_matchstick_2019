a8451ef7f62c6b44bda0928d836f4cbb9f5af1b5
gbogard <guillaume.bogard-coquard@epitech.eu>
2020-01-26 13:57:41 +0100

    Small changes: Removed an unuseful 'return' key word inside of the two_lines_left.c file

--- src/turns_actions/special_ai_choices/two_lines_left.c
+++ src/turns_actions/special_ai_choices/two_lines_left.c
@@ -12,9 +12,8 @@ void two_lines_left(lines_t **head, const info_t conditions, ai_move_t *choice)
     int matches_up = get_x_line(head, choice->pos_lines[0])->matches;
     int matches_down = get_x_line(head, choice->pos_lines[1])->matches;
 
-    if (matches_up == 1 || matches_down == 1) {
+    if (matches_up == 1 || matches_down == 1)
         one_at_one(head, conditions.max_to_remove, choice);
-        return;
-    } else
+    else
         random_input(head, conditions, choice);
 }
